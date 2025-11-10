0314eb37d691783af8578e8886c245a9aca0efdc
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-11-10 18:59:50 +0100

    Create QuickNetStats.rb

---
+++ Casks/QuickNetStats.rb
@@ -0,0 +1,14 @@
+cask "QuickNetStats" do
+  version "1.0.0-beta.1"
+
+  sha256 "sha256:da1704e3e54ffd83f73ba9cc7d45438632241a217129bcc4d199989b69fe8e25"
+
+  url "https://github.com/FI-153/QuickNetStats/releases/download/v1.0.0-beta.1/QuickNetStats.app.zip"
+
+  name "QuickNetStats"
+
+  homepage "https://github.com/FI-153/QuickNetStats"
+
+  app "QuickNetStats.app"
+
+end
