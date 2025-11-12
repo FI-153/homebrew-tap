92e30a4129a30f2ec28e44384fcc3e909e217cd6
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-11-12 11:29:54 +0100

    Create remindy.rb

---
+++ Casks/remindy.rb
@@ -0,0 +1,8 @@
+cask "remindy" do
+  version "2.0.0"
+  sha256 "sha256:6726b2e06c5f78adcc09fa287f7b7fcc0abaf53a7cc8a809906662bf558c0b58"
+  url "https://github.com/FI-153/Remindy/releases/download/v2.0.0/Remindy.app.zip"
+  name "Remindy"
+  homepage "https://github.com/FI-153/Remindy"
+  app "Remindy.app"
+end
