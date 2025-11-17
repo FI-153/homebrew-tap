61a1853de049c1657609eaba79baf52f7e35bd8f
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-11-17 20:00:32 +0100

    Update QuickNetStats to version 1.0.0-beta.3

--- Casks/quicknetstats.rb
+++ Casks/quicknetstats.rb
@@ -1,7 +1,7 @@
 cask "quicknetstats" do
-  version "1.0.0-beta.2"
-  sha256 "680894235c769d04c215cbae046ca62a9d9b4221d16645a9c37784d531e49396"
-  url "https://github.com/FI-153/QuickNetStats/releases/download/v1.0.0-beta.2/QuickNetStats.app.zip"
+  version "1.0.0-beta.3"
+  sha256 "032ea9bf70e2f4cdfd4af8e5b6f72567f0924b04a8ee60b402e25c10b4804cea"
+  url "https://github.com/FI-153/QuickNetStats/releases/download/v1.0.0-beta.3/QuickNetStats.app.zip"
   name "QuickNetStats"
   homepage "https://github.com/FI-153/QuickNetStats"
   app "QuickNetStats.app"
