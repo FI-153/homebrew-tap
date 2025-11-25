63d1039439209beb718268b190c46d5a71b70f30
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-11-25 11:36:55 +0100

    Update URL to version 1.0.0-beta.4

--- Casks/quicknetstats.rb
+++ Casks/quicknetstats.rb
@@ -1,7 +1,7 @@
 cask "quicknetstats" do
   version "1.0.0-beta.4"
   sha256 "7dddb24615443d1768c935eb5a3412c1078106d8b24c58ad96973ae6ce82adbe"
-  url "https://github.com/FI-153/QuickNetStats/releases/download/v1.0.0-beta.3/QuickNetStats.app.zip"
+  url "https://github.com/FI-153/QuickNetStats/releases/download/v1.0.0-beta.4/QuickNetStats.app.zip"
   name "QuickNetStats"
   homepage "https://github.com/FI-153/QuickNetStats"
   app "QuickNetStats.app"
