779213f10081651b471660cf11425dc77eb2807c
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-11-30 14:57:42 +0100

    Update sha256 checksum for QuickNetStats

--- Casks/quicknetstats.rb
+++ Casks/quicknetstats.rb
@@ -1,6 +1,6 @@
 cask "quicknetstats" do
   version "1.2.0"
-  sha256 "3b33fe3791afb1988b8098e429d5af60a3606d3542780f79b940bb91c7936c6f"
+  sha256 "b8f6abf875649b1615a2f263b2196ee73a16fac33bd6d09cb34ab0a16384be9e"
   url "https://github.com/FI-153/QuickNetStats/releases/download/V1.2.0/QuickNetStats.app.zip"
   name "QuickNetStats"
   homepage "https://github.com/FI-153/QuickNetStats"
