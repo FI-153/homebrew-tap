5a292c3c0d348d216b057e1976d3995f64cd1150
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-12-01 11:17:02 +0100

    Fixed icon

--- Casks/remindy.rb
+++ Casks/remindy.rb
@@ -1,6 +1,6 @@
 cask "remindy" do
   version "2.0.0"
-  sha256 "134f33a642045feb7fd2df38d8e665505ae4ae57639e5bcde5a67b720882c8e5"
+  sha256 "4bf1c0b2b03b0906a27eaeeadd08054227f2d8de422f0acc4b4d6f53fa732526"
   url "https://github.com/FI-153/Remindy/releases/download/v2.0.0/Remindy.app.zip"
   name "Remindy"
   homepage "https://github.com/FI-153/Remindy"
