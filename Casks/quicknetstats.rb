1e14108222c9390ce3e886302d97489cf5461179
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2025-12-14 10:40:13 +0100

    Update QuickNetStats to version 1.2.1

--- Casks/quicknetstats.rb
+++ Casks/quicknetstats.rb
@@ -1,7 +1,7 @@
 cask "quicknetstats" do
-  version "1.2.0"
-  sha256 "b8f6abf875649b1615a2f263b2196ee73a16fac33bd6d09cb34ab0a16384be9e"
-  url "https://github.com/FI-153/QuickNetStats/releases/download/V1.2.0/QuickNetStats.app.zip"
+  version "1.2.1"
+  sha256 "b62f1706faa64b35b891f0a43b211e03d9df34b38d4f00037b29cf80d25beebc"
+  url "https://github.com/FI-153/QuickNetStats/releases/download/V1.2.1/QuickNetStats.app.zip"
   name "QuickNetStats"
   homepage "https://github.com/FI-153/QuickNetStats"
   app "QuickNetStats.app"
