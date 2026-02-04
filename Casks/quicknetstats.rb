409c4afabf86fff7b355d697898b8a3a25889000
Federico Imberti <66013290+FI-153@users.noreply.github.com>
2026-02-04 20:58:35 +0100

    Fix formatting of version, sha256, and url

--- Casks/quicknetstats.rb
+++ Casks/quicknetstats.rb
@@ -1,7 +1,7 @@
 cask "quicknetstats" do
-  version .2.1.0
-  sha256 e9365da5dc614cdf6c206b07ecf087914a19c8309881f1d6eff69033c9ca32de
-  url https://github.com/FI-153/QuickNetStats/releases/download/V.2.1.0/QuickNetStats.app.zip
+  version "2.1.0"
+  sha256 "e9365da5dc614cdf6c206b07ecf087914a19c8309881f1d6eff69033c9ca32de"
+  url "https://github.com/FI-153/QuickNetStats/releases/download/V.2.1.0/QuickNetStats.app.zip"
   name "QuickNetStats"
   homepage "https://github.com/FI-153/QuickNetStats"
   app "QuickNetStats.app"
